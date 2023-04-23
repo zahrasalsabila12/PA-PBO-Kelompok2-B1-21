e717d5913639cbbd2b6d87861690d0530ed80d84
Daviana Dwitasari Enka Fornia <davianadwitsr178@gmail.com>
2023-04-23 13:36:37 +0800

    PA PBO

--- PA/src/pa/Main.java
+++ PA/src/pa/Main.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main{
     static ArrayList<Skincare> data1 = new ArrayList<>();
